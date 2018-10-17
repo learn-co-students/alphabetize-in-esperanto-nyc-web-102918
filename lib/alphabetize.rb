6cb527731051c3a844bd86a69f505f37231026ca
Jacob Menashi <jbmenashi@gmail.com>
2018-10-17 20:22:24 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |phrase| # sorting by each phrase
+ 	  phrase.split("").collect do |letter| # splits the phrase into individual letters, then creates a new array with collect that goes through each letter...
+      alphabet.index(letter) # ...and finds their index in the new alphabet. Then it sorts them by that index
+    end
+  end
 end
